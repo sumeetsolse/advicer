359ac89378426e8143da5199846d72909bf497da
sumeetsolse <sumeetsolse19@gmail.com>
2024-11-17 14:25:47 +0530

    added stateless widget to temp delete for checking

---
+++ lib/2_application/features/temp_delete_git_testing.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TempDeleteGitTesting extends StatelessWidget {
+  const TempDeleteGitTesting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
